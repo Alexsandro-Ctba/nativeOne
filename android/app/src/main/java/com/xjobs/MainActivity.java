e15b2b0067cdba5050fdcbd7cd4b865fa0f7a425
alexsandro <alexsandro.nv02@gmail.com>
2021-11-03 23:17:21 -0300

    reiniciado o projeto react-native

--- android/app/src/main/java/com/xjobs/MainActivity.java
+++ android/app/src/main/java/com/xjobs/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nativeone;
+package com.xjobs;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "nativeOne";
+    return "xjobs";
   }
 }
--- ios/xjobsTests/xjobsTests.m
+++ ios/xjobsTests/xjobsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nativeOneTests : XCTestCase
+@interface xjobsTests : XCTestCase
 
 @end
 
-@implementation nativeOneTests
+@implementation xjobsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
