f18c3329f2956bfbeb2b0d27e04db286c8094c73
alexsandro <alexsandro.nv02@gmail.com>
2021-11-15 13:04:00 -0300

    estudado useState()

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,8 +1,20 @@
-import React from "react";
-import {View, Text, StyleSheet, TextInput, Platform, TouchableOpacity} from 'react-native';
+import React, { useState } from "react";
+import {
+  Platform, StyleSheet, Text, TextInput, TouchableOpacity, View
+} from 'react-native';
 
 
 export default function Home(){
+  // armazena apenas 1 skill
+  const [newSkill, setNewSkill] = useState();
+   //armazena todas as skills 
+   const [mySkills, setMySkills] = useState([]);
+
+   //usar handle quando o processo partir do clique de usuários
+   function handleAddNewSkill(){
+      setMySkills(oldState => [...mySkills, newSkill])
+   }
+
   return(
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -13,14 +25,32 @@ export default function Home(){
       style={styles.input}
       placeholder="insira o valor aqui"
       placeholderTextColor="#555"
+      onChangeText={setNewSkill}
       />
       {/* exibindo botão = button
         activeOpacity = controlando a opacidade no clique
       */}
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}
+        onPressIn={handleAddNewSkill}
+      >
       <Text style={styles.buttonText}>Add</Text>
+      
       </TouchableOpacity>
       
+      {/* colocando margin de 50 em cima e amabixo */}
+      <Text style={[styles.title, {marginVertical:50}]}>
+        My Skills
+      </Text>
+
+ {     
+  mySkills.map(skills  =>(
+    <TouchableOpacity key={skills} style={styles.buttonSkill}>
+    <Text style={[styles.textSkill]}>
+        {skills}
+    </Text> 
+    </TouchableOpacity>
+  ))
+}
     </View>
   )
 }
@@ -54,5 +84,18 @@ const styles = StyleSheet.create({
   },
   buttonText:{
     color:'#fff',fontSize:17, fontWeight:'bold'
+  },
+
+  textSkill:{
+    color:'#fff', fontSize:22, 
+    fontWeight:'bold',
+   
+  },
+  buttonSkill:{
+    borderRadius:50,
+    padding:15,
+    backgroundColor:'#1f1e25', 
+    alignItems:'center',
+    marginVertical:3
   }
 })
