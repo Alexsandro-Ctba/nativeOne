2a0ab583ac3967eb841c639ddaeed151f90c063f
alexsandro <alexsandro.nv02@gmail.com>
2021-12-04 23:31:27 -0300

    atualizado tela inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   FlatList,
-  Platform, StyleSheet, Text,
+  Platform, StatusBar, StyleSheet, Text,
   TextInput, View
 } from "react-native";
 import { Button } from "./components/Button";
@@ -11,17 +11,28 @@ import { Skillcard } from "./components/Skillcard";
 export function Home() {
   const [newSkill, setNewSkill] = useState('valor padrão');
   const [mySkill, setMyskill] = useState([]);
-
+  const [greeting, setGreeting] = useState('');
 
   function handleAddNewSkill() {
     //oldstate recupera os dados que já estavam dentro de MySkill
     setMyskill(oldstate => [...oldstate, newSkill]);
   }
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+    if (currentHour < 12) {
+      setGreeting('Bom dia')
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting('Boa noite')
+    }
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pagina inicial</Text>
-
+     
+      <Text style={styles.title}>Bem vindo!</Text>
+      <Text style={styles.greetings}>
+        {greeting}
+      </Text>
       <TextInput style={styles.input}
         placeholder="digite aqui"
         placeholderTextColor="#555"
@@ -33,13 +44,15 @@ export function Home() {
       <Text style={[styles.title, { marginVertical: 50 }]}>
         My Skills
       </Text>
-      <FlatList 
-      
-      data={mySkill}
-      keyExtractor={item=>item}
-      renderItem={({item}) =>(
-        <Skillcard item={item}/>
-      )}
+
+
+      <FlatList
+
+        data={mySkill}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Skillcard item={item} />
+        )}
       />
     </View>
 
@@ -66,5 +79,8 @@ const styles = StyleSheet.create({
     marginTop: 30,
     borderRadius: 7
   },
+  greetings: {
+    color: '#fff'
+  }
 
 })
